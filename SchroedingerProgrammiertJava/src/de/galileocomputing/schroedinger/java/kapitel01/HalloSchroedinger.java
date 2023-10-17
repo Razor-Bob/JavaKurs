1136ca61a02bc30b993293be77c6caacee03e561
Razor-Bob <razorbob007@googlemail.com>
2023-10-17 13:35:57 +0200

    Kapitel 1 & erstes Programm
    Hallo Schrödinger

---
+++ SchroedingerProgrammiertJava/src/de/galileocomputing/schroedinger/java/kapitel01/HalloSchroedinger.java
@@ -0,0 +1,10 @@
+package de.galileocomputing.schroedinger.java.kapitel01;
+
+public class HalloSchroedinger {
+
+	public static void main(String[] args) {
+		System.out.println("HalloSchrodinger");
+
+	}
+
+}
