02ef06ea2004ead9f95ebca2abf576a7e0ac49a7
Razor-Bob <razorbob007@googlemail.com>
2023-10-18 14:44:46 +0200

    Erstellen Eingabeströme
    Eingabe = 1
    Ausgabe = 49
    
    Sinn?

---
+++ SchroedingerProgrammiertJava/src/de/galileocomputing/schroedinger/java/kapitel01/Eingabestroeme.java
@@ -0,0 +1,14 @@
+package de.galileocomputing.schroedinger.java.kapitel01;
+import java.io.IOException;
+
+public class Eingabestroeme {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("Hallo " + args[0]);
+		System.out.println("Gib einen Wert ein:");
+		int wert = System.in.read();
+		System.out.println(wert);
+
+	}
+
+}
