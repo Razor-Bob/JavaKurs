786d9c29eebff1babae28a3e8efd78b6ee3e365c
Razor-Bob <razorbob007@googlemail.com>
2024-01-13 20:41:12 +0100

    Create ErhöhungdesWerteseinerVariablen.java

---
+++ LearnJ/src/variablen/ErhöhungdesWerteseinerVariablen.java
@@ -0,0 +1,15 @@
+package variablen;
+
+public class ErhöhungdesWerteseinerVariablen {
+
+	public static void main(String[] args) {
+		int a; // Deklaration der Variable a
+		a=37; // Wertzuweisung
+		
+		a=a+1; // Erhöht den Wert der Variablen um 1
+		
+		System.out.println(a);
+
+	}
+
+}
