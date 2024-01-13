5110662cd071b07411dfa33f6ae7b2d6e9e11e59
Razor-Bob <razorbob007@googlemail.com>
2024-01-13 20:23:22 +0100

    New Project
    LearnJ

---
+++ LearnJ/src/variablen/WertzuweisungBerechnungvonTermwerten.java
@@ -0,0 +1,20 @@
+package variablen;
+
+public class WertzuweisungBerechnungvonTermwerten {
+
+	public static void main(String[] args) {
+		
+		int a;	// Deklaration der Variable
+		a =12;	// Wertzuweisung
+		
+		// Auswertung von Termen
+		System.out.println(a);
+		System.out.println(3*a+5);
+		
+		// Im Unterschied dazu: Ausgabe einer Zeichenkette:
+		System.out.println("a");
+		System.out.println("3*a+5");
+	
+	}
+
+}
--- SchroedingerProgrammiertJava/src/de/galileocomputing/schroedinger/java/kapitel01/HalloSchroedinger.java
+++ SchroedingerProgrammiertJava/src/de/galileocomputing/schroedinger/java/kapitel01/HalloSchroedinger.java
@@ -3,7 +3,7 @@ package de.galileocomputing.schroedinger.java.kapitel01;
 public class HalloSchroedinger {
 
 	public static void main(String[] args) {
-		System.out.println("Hallo " + args[0]);
+		System.out.println("Hallo Welt");
 // Github-Test Mac
 // Github-Test iPhone
 	}
