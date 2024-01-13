e41c4b757eaff89f4adbd530ac577d316c415607
Razor-Bob <razorbob007@googlemail.com>
2024-01-13 22:34:26 +0100

    Programm auf Anfang

---
+++ LearnJ/src/variablen/ErhöhungdesWerteseinerVariablen2.java
@@ -0,0 +1,24 @@
+package variablen;
+
+public class ErhöhungdesWerteseinerVariablen2 {
+
+	public static void main(String[] args) {
+		
+		int a; // Deklaration der Variable a
+		a=12;
+		
+		a+=7; // Der Wert von a wird um 7 erhöht.
+		System.out.println(a);
+		
+		a--; // Der Wert von a wird um 1 verringert.
+		System.out.println(a);
+		
+		a++; // Der Wert von a wird um 1 erhöht.
+		System.out.println(a);
+		
+		a=a*3; // Der Wert von a wird verdreifacht.
+		System.out.println(a);
+
+	}
+
+}
