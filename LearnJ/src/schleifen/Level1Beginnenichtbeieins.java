6fb86712f7c3095f33f2c45f6137a03c2171e762
Razor-Bob <razorbob007@googlemail.com>
2024-01-16 20:43:57 +0100

    Aufgaben
    While Schleifen

---
+++ LearnJ/src/schleifen/Level1Beginnenichtbeieins.java
@@ -0,0 +1,21 @@
+package schleifen;
+
+public class Level1Beginnenichtbeieins {
+
+	// Schreibe ein Programm, das von fünf bis 25 zählt und die Zahl jeweils ausgibt, also
+	// 5, 6, 7, 8, ...
+	
+	public static void main(String[] args) {
+		int n=5;
+		
+		while (n<=25) {
+			
+			System.out.println(n);
+			
+			n=n+1;
+			
+		}
+	// alleine geschafft :-)
+	}
+
+}
