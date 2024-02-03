846655ad1b45407c7c7707740d74ca569facb7d9
Razor-Bob <razorbob007@googlemail.com>
2024-02-03 22:33:53 +0100

    Beispiel 5
    Die while-loop mit Endbedingung (Wiederholung mit Endbedingung)

---
+++ LearnJ/src/schleifen/WhilemitEndbedingungBeispiel5.java
@@ -0,0 +1,23 @@
+package schleifen;
+
+public class WhilemitEndbedingungBeispiel5 {
+
+	// Man kann die while-loop auch so schreiben, dass die Bedingung zum Fortfahren jeweils erst am 
+	// Ende jedes Wiederholungsschrittes überprüft wird.
+	// while-loop mit Endbedingung (do...while-loop)
+	// Wir zählen von 1 bis 10
+	
+	public static void main(String[] args) {
+		
+		int i=0;
+		
+		do {
+			
+			System.out.println(i);
+			i++;
+			
+		} while (i<=10);	
+
+	}
+
+}
